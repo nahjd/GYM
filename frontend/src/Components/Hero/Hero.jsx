58cf850632e7569a8f9c2911fe6cffdcc8ba6d47
nahjd <nahidmemmedov711@gmail.com>
2024-08-07 12:23:15 +0400

    swiper hover

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -8,8 +8,21 @@ import './Hero.scss';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 function Hero() {
+    const [animationClass, setAnimationClass] = useState('text1 animate');
+
+    const handleSlideChange = () => {
+        setAnimationClass('text1');
+        setTimeout(() => {
+            setAnimationClass('text1 animate');
+        }, 10);
+    };
+
+    useEffect(() => {
+        handleSlideChange();
+    }, []);
+
     return (
-        <div >
+        <div>
             <Swiper
                 spaceBetween={0}
                 autoplay={{
@@ -18,63 +31,46 @@ function Hero() {
                 }}
                 pagination={{
                     clickable: true,
+                    renderBullet: (index, className) => `<span class="${className}">${index + 1}</span>`,
                 }}
+                onSlideChange={handleSlideChange}
                 modules={[Autoplay, Pagination, Navigation]}
                 className="mySwiper"
             >
                 <SwiperSlide>
                     <div className="hero">
                         <div className="main">
-                            <div className="text1">
-                                <h1> JOIN US ON OUR <br />FITNESS JOURNEY   </h1>
-
-
+                            <div className={animationClass}>
+                                <h1>JOIN US ON OUR <br />FITNESS JOURNEY</h1>
                                 <p>Lorem ipsum dolor sit amet.</p> <br />
-                                <button>
-                                    READ MORE
-                                </button>
+                                <button>READ MORE</button>
                             </div>
                         </div>
-
                     </div>
-
-
-
-
                 </SwiperSlide>
                 <SwiperSlide>
-
                     <div className="hero hero1">
                         <div className="main">
-                            <div className="text1">
-                                <h1> JOIN US ON OUR <br />FITNESS JOURNEY   </h1>
-
-
+                            <div className={animationClass}>
+                                <h1>JOIN US ON OUR <br />FITNESS JOURNEY</h1>
                                 <p>Lorem ipsum dolor sit amet.</p> <br />
-                                <button>
-                                    READ MORE
-                                </button>
+                                <button>READ MORE</button>
                             </div>
                         </div>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide> <div className="hero hero2">
-                    <div className="main">
-                        <div className="text1">
-                            <h1> JOIN US ON OUR <br />FITNESS JOURNEY   </h1>
-
-
-                            <p>Lorem ipsum dolor sit amet.</p> <br />
-                            <button>
-                                READ MORE
-                            </button>
+                <SwiperSlide>
+                    <div className="hero hero2">
+                        <div className="main">
+                            <div className={animationClass}>
+                                <h1>JOIN US ON OUR <br />FITNESS JOURNEY</h1>
+                                <p>Lorem ipsum dolor sit amet.</p> <br />
+                                <button>READ MORE</button>
+                            </div>
                         </div>
                     </div>
-                </div>
                 </SwiperSlide>
-
             </Swiper>
-
         </div>
     );
 }
