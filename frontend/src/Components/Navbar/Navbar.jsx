9395388beb04221ad5c91efb30dd7604f77021b9
nahjd <nahidmemmedov711@gmail.com>
2024-08-21 12:46:40 +0400

    nav

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -7,9 +7,12 @@ import { MdOutlineLogin } from "react-icons/md";
 import { RiMenuFill, RiCloseFill } from 'react-icons/ri';
 import img1 from "./../../../images/navleft.png";
 import { RiAdminFill } from "react-icons/ri";
+import { useSelector } from 'react-redux';
 
 function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false);
+    const wishlistCount = useSelector((state) => state.gym.wishlist.length);
+    const basketCount = useSelector((state) => state.gym.basket.length);
 
     return (
         <div className="container">
@@ -26,8 +29,12 @@ function Navbar() {
                     <li><Link to="/blog">BLOG</Link></li>
                     <li><Link to="/portfolio">PORTFOLIO</Link></li>
                     <li><Link to="/shop">SHOP</Link></li>
-                    <li><Link to="/wishlist"><BiHeart style={{ color: "white", fontSize: "25px" }} /> </Link></li>
-                    <li>  <Link to="/basket" ><BiBasket style={{ color: "white", fontSize: "25px" }} /></Link></li>
+
+                    <Link to="/wishlist">
+                        <BiHeart style={{ color: "white", fontSize: "25px" }} />
+                        {wishlistCount > 0 && <span className="count-badge">{wishlistCount} </span>} </Link>
+                    <Link to="/basket" >  <BiBasket style={{ color: "white", fontSize: "25px" }} />
+                        {basketCount > 0 && <span className="count-badge">{basketCount}</span>}</Link>
 
 
 
